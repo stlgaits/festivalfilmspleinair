9a5845b467c23aba397ea8a2ab0c11db133b2937
EstelleMyddleware <estellegaits@myddleware.com>
2021-05-20 20:16:31 +0200

    dropdown nav burger menu on small devices

---
+++ functions.js
@@ -0,0 +1,17 @@
+
+
+var burger = document.getElementById("burger");
+var burgerNav = document.getElementById("burger-nav");
+
+burger.addEventListener('touchend', event => {
+    toggleMenu();
+});
+
+// Toggling dropdown menu when clicking on burger (only on mobile devices)
+function toggleMenu(){
+    if(burgerNav.hidden === true){
+        burgerNav.hidden = false;
+    } else {
+        burgerNav.hidden = true;
+    }
+}
